8081840c9f46488eb515dc04c91707b244d95fe6
shivambadoni19992 <shivambadoni.39@gmail.com>
2022-09-11 23:30:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ATM-system-simulator
+ 
---
+++ src/ASimulatorSystem/Conn.java
@@ -0,0 +1,18 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Abc02");    
+            s =c.createStatement(); 
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
